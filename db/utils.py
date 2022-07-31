0da19c79463b96baf5532cd6777b9fe5236d629e
emotzart <johnshuabratsk@gmail.com>
2022-07-31 16:18:24 +0700

    Fixed fill_db

--- db/utils.py
+++ db/utils.py
@@ -2,14 +2,12 @@ import json
 from db.db_init import db
 
 def fill_db(data_file: str, table_name: db.Model):
+    db.create_all()
     with open(data_file) as file:
         data = json.load(file)
 
     list_to_append = [table_name(**kwarg) for kwarg in data]
-    for item in list_to_append:
-        db.session.add(item)
-        db.session.commit()
-    # db.session.add_all(list_to_append)
-    # db.session.commit()
+    db.session.add_all(list_to_append)
+    db.session.commit()
     print(db.session.query(table_name).all())
     db.session.close()
