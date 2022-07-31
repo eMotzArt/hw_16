86ad3897c7b7a6319e5e72c56c94dee6b2fd13d3
emotzart <johnshuabratsk@gmail.com>
2022-07-31 18:02:51 +0700

    add routes for all_user and user_by_id

--- app/blueprints/views.py
+++ app/blueprints/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, url_for, redirect, jsonify
 from db.utils import fill_db
 from db.models import *
+from app.blueprints.dao.utils import get_all_users_list, get_user_by_id
 
 
 main_blueprint = Blueprint('main_blueprint', __name__)
@@ -12,6 +13,11 @@ def init_db():
     fill_db('db/data/orders.json', Order)
     fill_db('db/data/offers.json', Offer)
 
-@main_blueprint.route('/')
-def main_page():
-    return jsonify(db.session.query(Order).first().to_dict())
+@main_blueprint.route('/users/')
+def page_all_users():
+    return jsonify(get_all_users_list())
+    # return db.session.query(Order, User).join((User, Order.customer_id == User.id)).all()
+
+@main_blueprint.route('/users/<int:user_id>')
+def page_user_by_id(user_id):
+    return jsonify(get_user_by_id(user_id))
