4a4eecda65547f7e0665ce49ac4913a7db08f22d
emotzart <johnshuabratsk@gmail.com>
2022-07-31 16:19:03 +0700

    added db filling in flask_app

--- app/blueprints/views.py
+++ app/blueprints/views.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, request, url_for, redirect, jsonify
+from db.utils import fill_db
+from db.models import *
+
 
 main_blueprint = Blueprint('main_blueprint', __name__)
 
 
-
 @main_blueprint.before_app_first_request
-def init():
-
-    ...
+def init_db():
+    fill_db('db/data/users.json', User)
+    fill_db('db/data/orders.json', Order)
+    fill_db('db/data/offers.json', Offer)
 
 @main_blueprint.route('/')
 def main_page():
-    return 'OK'
+    return jsonify(db.session.query(Order).first().to_dict())
