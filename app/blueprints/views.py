09bdc6b34135296bd2f7bb8c5e642c158dd5d6d8
emotzart <johnshuabratsk@gmail.com>
2022-07-31 19:07:20 +0700

    add order and offers routes

--- app/blueprints/views.py
+++ app/blueprints/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request, url_for, redirect, jsonify
 from db.utils import fill_db
 from db.models import *
-from app.blueprints.dao.utils import get_all_users_list, get_user_by_id
-
+from app.blueprints.dao.utils import get_all_users_list, get_user_by_id, get_all_orders_list, get_order_by_id, \
+    get_all_offers_list, get_offer_by_id
 
 main_blueprint = Blueprint('main_blueprint', __name__)
 
@@ -18,6 +18,27 @@ def page_all_users():
     return jsonify(get_all_users_list())
     # return db.session.query(Order, User).join((User, Order.customer_id == User.id)).all()
 
-@main_blueprint.route('/users/<int:user_id>')
+@main_blueprint.get('/users/<int:user_id>')
 def page_user_by_id(user_id):
     return jsonify(get_user_by_id(user_id))
+
+@main_blueprint.get('/orders/')
+def page_all_orders():
+    return jsonify(get_all_orders_list())
+
+@main_blueprint.get('/orders/<int:order_id>')
+def page_order_by_id(order_id):
+    return jsonify(get_order_by_id(order_id))
+
+@main_blueprint.get('/offers/')
+def page_all_offers():
+    return jsonify(get_all_offers_list())
+
+@main_blueprint.get('/offers/<int:offer_id>')
+def page_offer_by_id(offer_id):
+    return jsonify(get_offer_by_id(offer_id))
+
+@main_blueprint.post('/users/')
+def add_new_user():
+    ...
+    # return jsonify(get_all_users_list())
